1a3f0f67e49ce5ba9c069f354cd7b9bc56f5e59d
Zachary Lau <zlau2000@gmail.com>
2025-02-22 20:38:34 -0700

    Add optimization and compare with uniform level affinities

--- example.py
+++ example.py
@@ -6,6 +6,9 @@ Syed et al.
 import numpy as np
 from mytest import Test
 from matplotlib import pyplot as plt
+import te
+from scipy.optimize import minimize, fsolve
+import stationary
 
 def find_one(dc, db, a, k, beta):
     """
@@ -54,10 +57,10 @@ def find_alphas(dc, a, k, beta):
     alpha[1,0] = 0 # down from 0
     alpha[0,n] = 0 # up from n 
     alpha[0,0] = find_one(dc[0], beta[1]-beta[0], a, k, beta[0]) # up from 0
-    alpha[1,n] = find_one(-dc[n-1], beta[n-1]-beta[n], a, k, beta[i])  # down from n
+    alpha[1,n] = find_one(-dc[n-1], beta[n-1]-beta[n], a, k, beta[n])  # down from n
     return alpha
  
-def test():
+def unit_test():
     tests = [
         Test("", np.allclose, find_one(1, 0.1, 2, 10, 0.1), 1)
     ]
@@ -79,12 +82,106 @@ def graph():
     plt.plot(dcs, down)
     plt.show()
 
-if __name__ == "__main__":
-    test()
+def two_layer_test(dc=[0,0], a = 2):
     # print(find_one(-1, -0.1, 2, 10, 0.1)) 
     # graph()
-    dc = [0,0]
+    # dc = [0,0]
     betas = [0, 0.5, 1]
+    k = 10
+    alpha = find_alphas(dc, a, k, betas)
+    # print(alpha)
+    tour_eff = te.theory_te(alpha[0,],alpha[1,])
+    print(tour_eff)
+
+def n_layer_test(n):
+    dc = np.zeros(n)
+    betas = np.linspace(start=0, stop=1, num=n+1)
     a = 2
     k = 10
-    print(find_alphas(dc, a, k, betas))
+    alpha = find_alphas(dc, a, k, betas)
+    # print(alpha)
+    tour_eff = te.theory_te(alpha[0,],alpha[1,])
+    print(tour_eff)
+
+def optimize_grid():
+    betas = [0, 0.5, 1]
+    a = 10
+    k = 10
+    xs = np.linspace(-2*np.log(a), 2*np.log(a))
+    ys = xs.copy() # deep copy
+    zs = np.zeros((len(xs), len(ys)))
+    for i,x in enumerate(xs):
+        for j,y in enumerate(ys):
+            alpha = find_alphas([x,y], a, k, betas)
+            zs[j,i] = te.theory_te(alpha[0,],alpha[1,])
+    cs = plt.contour(xs, ys, zs)
+    plt.clabel(cs)
+    plt.show()
+
+def optimize_test():
+    dcs = [-1,-2]
+    betas = [0, 0.5, 1]
+    a = 10
+    k = 10
+    # def to_opt
+    def f(x):
+        alpha = find_alphas(x, a, k, betas)
+        tour_eff = te.theory_te(alpha[0,],alpha[1,])
+        return tour_eff
+    # Nelder-mead often flops
+    # res = minimize(lambda x : -f(x), dcs, method="Nelder-Mead")
+    # Default is BFGS : performs reasonably well in this case
+    res = minimize(lambda x : -f(x), dcs)
+    print(res)
+    print(f(res.x))
+
+def find_uniform_affinities():
+    print("==== Beginning calculation for uniform affinity tuning params === ")
+    a = 10
+    k = 10
+    betas = [0, 0.5, 1]
+    # We could solve a systme of equations, but its easier to just match
+    # upwards and dowanrds rates
+    n = len(betas)-1 # number of things to solve for
+    cs = [0]*n
+    for i in range(n):
+        db = betas[i+1]-betas[i]
+        def f(dc):
+            up = find_one(dc, db, a, k, betas[i])
+            down = find_one(-dc, -db, a, k, betas[i+1])
+            return up-down
+        cs[i] = fsolve(f, -1)[0]
+        # print(f(cs[i]))
+        # print(find_one(cs[i], db, a, k, betas[i]))
+        # print(find_one(-cs[i], -db, a, k, betas[i+1]))
+    print(f"The uniform level affinites are given by: {cs}")
+    alphas = find_alphas(cs, a, k, betas)
+    print(f"We get acceptance rates: {alphas}")
+    sprobs = stationary.stationary_probs(alphas[0,], alphas[1,]).sum(axis=0)
+    print(f"The stationary probabilties are: {sprobs}")
+    myte = te.theory_te(alphas[0,], alphas[1,])
+    print(f"The te is: {myte}")
+
+def explore_optimized_region():
+    dcs = [0, -1.151]
+    a = 10
+    k = 10
+    betas = [0, 0.5, 1]
+    alphas = find_alphas(dcs, a, k, betas)
+    print(f"The acceptance rates are: {alphas}")
+    sprobs = stationary.stationary_probs(alphas[0,], alphas[1,]).sum(axis=0)
+    print(f"The level affinities are: {sprobs}") # these are non statinoary!
+
+if __name__ == "__main__":
+    unit_test()
+    # graph()
+    optimize_grid() # returns near (0,1)
+    # two_layer_test([0,0]) # 0.744
+    # two_layer_test([-0.303, -0.055]) # 0.712
+    # two_layer_test([0,0], a = 10) # 0.548
+    # two_layer_test([4,4], a = 10) # 0.5 (i.e. p up = 1 p down = 0)
+    # two_layer_test([0, -1], a = 10) # 0.596
+    # n_layer_test(10) # 0.744
+    optimize_test() # gives best result at 0, -1.151 with value 0.619
+    explore_optimized_region()
+    find_uniform_affinities()
